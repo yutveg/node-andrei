047184bfaa1a0ed8236154760ea4c714f3018160
yutveg <kodi.0n31l@gmail.com>
2020-08-12 15:42:01 -0700

    very basic middleware

--- server.js
+++ server.js
@@ -2,4 +2,18 @@ const express = require('express');
 
 const app = express();
 
+
+app.use((req, res, next) => {
+    console.log(req)
+    next();
+})
+
+app.get('/', (req, res) => {
+    const user = {
+        name: "Sally", 
+        hobby: "Painting"
+    }
+    res.send(user)
+})
+
 app.listen(3000);
