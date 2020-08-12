c2fcd1fa5aa0254011960eff18b62c1592a61d0f
yutveg <kodi.0n31l@gmail.com>
2020-08-11 17:17:26 -0700

    change to readme and logging request method and url

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const http = require('http')
 
 const server = http.createServer((request, response) => {
-    console.log(request)
+    console.log("method", request.method)
+    console.log("url", request.url)
     response.setHeader('Content-Type', 'text/html');
-    response.end("<h1>Hellooooo</h1><h2>Coom</h2>")
+    response.end("<h1>Hellooooo</h1>")
 })
 
 server.listen(3000);
