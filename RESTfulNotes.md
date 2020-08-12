f17f733f2548e52c6ec74bc147d06881875bbfc5
yutveg <kodi.0n31l@gmail.com>
2020-08-12 16:35:06 -0700

    serving static files

---
+++ RESTfulNotes.md
@@ -0,0 +1,13 @@
+# Defines a set of functions that perform operations for a request. 
+
+### Is an architectural style- an agreed upon set of rules for servers.
+
+### CRUD methods
+- post
+- get
+- put
+- delete 
+
+### Stateless
+- calls can be made independent of each other
+- each call contains all data necessary for itself
--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const express = require('express');
 const app = express();
 
 
-app.use((req, res, next) => {
-    console.log(req)
-    next();
-})
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    const user = {
-        name: "Sally", 
-        hobby: "Painting"
-    }
-    res.send(user)
+//console.log(req.query) -> access url query string /?<key>:<value>&<key>:<value>
+//console.log(req.body) -> access whatever data sent on request
+//console.log(req.headers) -> access browser headers
+//console.log(req.params) -> access params sent on url /:someResourceIdentifier
+
+app.use(express.static(__dirname + '/public'))
+
+app.post('/profile', (req, res) => {
+    res.send(req.body)
 })
 
 app.listen(3000);
